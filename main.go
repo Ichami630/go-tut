ed23cdb4d606810807fca63d589e3c6938f665be
ichami630 <brandonichami@gmail.com>
2025-02-21 12:11:44 +0100

    variables in go

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+var name = "ichami"      //type inference
+var city string = "buea" //explicit type declaration
+var occupation string
+
 func main() {
-	fmt.Println("Hello, Go, ichami is coming for you")
+	country := "cameroon" //short declaration, only allowed inside functions
+	fmt.Println("hello, go, ichami is coming for you!")
+	fmt.Println("Country:", country)
 }
