53c6e27c33fae80d232ff19fa248928977549c3b
ichami630 <brandonichami@gmail.com>
2025-02-26 10:50:27 +0100

    structs and custom types

--- main.go
+++ main.go
@@ -1,45 +1,64 @@
 package main
 
-/*Pointers in Go
-A pointer is a variable that stores the memory location of anothe variable
-Pointers help us modify variables outside the variable scope and efficiently manage memory
-Use an *(asterisk) to declare a pointer and &(ampersand) to get the address of the variable
-*/
+/*Structs and custom types in Go
+Go doesn't have classes, but structs allow you to create custom data types that can group related fields together
+A struct is a collection of related fields
 
+-Struct with pointers
+*By default, Go passes copy of struct to functions. To modify a struct, use pointers
+*/
 import "fmt"
 
-func updateName(x *string) { //creates a pointer variable of type string
-	*x = "brandon" //dereferencing the pointer to modify the original value
-	/*Now what does this mean?
-	Instead of changing x itself, which is just an address,we go to that memory location
-	We modify the actuall value which is stored at that memory location
-	*/
+// define a struct
+type Person struct {
+	name string
+	age  int
 }
 
-func changeNum(x *int) {
-	*x = 100
+type Car struct {
+	brand string
+	model string
+	year  int
 }
+
+type Book struct {
+	title  string
+	author string
+}
+
+// method for car struct
+/*A method is tied to a specific struct type and can access it fields without exlicitly passing them
+Receiver parameter (c Car)
+c is an instance of Car pass implicitly
+*/
+func (c Car) displayInfo() {
+	fmt.Printf("Car: %s %s (%d)\n", c.brand, c.model, c.year)
+}
+
+// method using pointer receiver
+func (b *Book) updateTitle(newTitle string) {
+	b.title = newTitle
+}
+
+// custom types
+// you can define a custom type using type
+type Age int
 
 func main() {
-	name := "ichami" //declare normal variables
-	num := 10
+	//create a struct instance
+	p1 := Person{name: "Ichami", age: 10}
+	//access fields
+	fmt.Println("Name:", p1.name, "\nAge:", p1.age)
 
-	//declaring a pointer to an integer
-	var ptr *int
+	//struct methods
+	c1 := Car{brand: "Benz", model: "ML350", year: 2025}
+	c1.displayInfo()
 
-	ptr = &num //assigning the address of num to the ptr
-	changeNum(ptr)
+	b1 := Book{title: "Golang", author: "ichami"}
+	fmt.Println("before:", b1)
+	b1.updateTitle("Golang new edition")
+	fmt.Println("After:", b1)
 
-	// updateName(name)
-
-	// fmt.Println("memory address of name:", &name)
-
-	m := &name //pointer m that stores the memory address of the variable name
-	// fmt.Println("memory address:", m)
-
-	// fmt.Println("value stored at pointer memory address m:", m, "is the original value of name:", *m) //the * is used to derefference a pointer
-	//Dereferencing a pointer means goining into to the memory address of that pointer and getting the value which the pointer points to
-	fmt.Println(name) //original value of name remains unchanged
-	updateName(m)
-	fmt.Println("now the original value of the var name changes to the updated name as:", name)
+	var myAge Age = 10
+	fmt.Println(myAge)
 }
